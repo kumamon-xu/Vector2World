ba7ae950345e97ea486e295d387dda554d4100c7
Tobias Knerr <osm@tobias-knerr.de>
2026-08-14 14:32:04 +0200

    Include texture export in GltfOutputTest

--- core-jvm/src/test/java/org/osm2world/output/gltf/GltfOutputTest.java
+++ core-jvm/src/test/java/org/osm2world/output/gltf/GltfOutputTest.java
@@ -1,19 +1,22 @@
 package org.osm2world.output.gltf;
 
 import static org.osm2world.math.VectorXYZ.NULL_VECTOR;
+import static org.osm2world.output.common.ResourceOutputSettings.ResourceOutputMode;
 import static org.osm2world.scene.material.DefaultMaterials.STEEL;
 import static org.osm2world.util.test.TestFileUtil.createTempFile;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.Map;
 
 import org.junit.Test;
+import org.osm2world.conversion.O2WConfig;
 import org.osm2world.map_data.creation.MapDataBuilder;
 import org.osm2world.map_data.data.MapNode;
 import org.osm2world.scene.Scene;
 import org.osm2world.scene.material.Material;
 import org.osm2world.scene.mesh.ExtrusionGeometry;
 import org.osm2world.scene.mesh.Mesh;
+import org.osm2world.util.platform.image.ImageImplementationJvm;
 import org.osm2world.util.platform.json.JsonImplementationJvm;
 import org.osm2world.util.test.TestWorldModule;
 
@@ -21,39 +24,45 @@ public class GltfOutputTest {
 
 	static {
 		JsonImplementationJvm.register();
+		ImageImplementationJvm.register();
 	}
 
 	@Test
-	public void testSimpleGltf() throws IOException {
-		createTemporaryTestGltf(".gltf");
+	public void testSimpleGltf() {
+		createTemporaryTestGltfs(".gltf");
 	}
 
 	@Test
-	public void testSimpleGlb() throws IOException {
-		createTemporaryTestGltf(".glb");
+	public void testSimpleGlb() {
+		createTemporaryTestGltfs(".glb");
 	}
 
 	@Test
-	public void testSimpleGltfGz() throws IOException {
-		createTemporaryTestGltf(".gltf.gz");
+	public void testSimpleGltfGz() {
+		createTemporaryTestGltfs(".gltf.gz");
 	}
 
 	@Test
-	public void testSimpleGlbGz() throws IOException {
-		createTemporaryTestGltf(".glb.gz");
+	public void testSimpleGlbGz() {
+		createTemporaryTestGltfs(".glb.gz");
 	}
 
 	@Test
-	public void testSimpleGltfZip() throws IOException {
-		createTemporaryTestGltf(".gltf.zip");
+	public void testSimpleGltfZip() {
+		createTemporaryTestGltfs(".gltf.zip");
 	}
 
 	@Test
-	public void testSimpleGlbZip() throws IOException {
-		createTemporaryTestGltf(".glb.zip");
+	public void testSimpleGlbZip() {
+		createTemporaryTestGltfs(".glb.zip");
 	}
 
-	private static void createTemporaryTestGltf(String fileExtension) throws IOException {
+	private static void createTemporaryTestGltfs(String fileExtension) {
+		createTemporaryTestGltf(fileExtension, ResourceOutputMode.EMBED);
+		createTemporaryTestGltf(fileExtension, ResourceOutputMode.STORE_SEPARATELY_AND_REFERENCE);
+	}
+
+	private static void createTemporaryTestGltf(String fileExtension, ResourceOutputMode resourceOutputMode) {
 
 		File tempFile = createTempFile(fileExtension);
 
@@ -65,10 +74,17 @@ public class GltfOutputTest {
 		MapDataBuilder dataBuilder = new MapDataBuilder();
 		MapNode node = dataBuilder.createNode(0, 0);
 		node.addRepresentation(new TestWorldModule.TestNodeWorldObject(node, mesh));
+		MapNode nodeB = dataBuilder.createNode(0, 5);
+		nodeB.addRepresentation(new TestWorldModule.TestNodeWorldObject(node));
 
 		Scene scene = new Scene(null, dataBuilder.build());
 
+		O2WConfig config = new O2WConfig(Map.of(
+				"staticResourceOutputMode", resourceOutputMode.toString(),
+				"generatedResourceOutputMode", resourceOutputMode.toString()));
+
 		var target = new GltfOutput(tempFile);
+		target.setConfiguration(config);
 		target.outputScene(scene);
 
 	}
--- core/src/main/java/org/osm2world/util/test/TestWorldModule.java
+++ core/src/main/java/org/osm2world/util/test/TestWorldModule.java
@@ -1,8 +1,6 @@
 package org.osm2world.util.test;
 
-import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
-import static org.osm2world.scene.material.DefaultMaterials.PLASTIC;
 
 import java.util.List;
 import java.util.Map;
@@ -13,8 +11,14 @@ import org.osm2world.map_data.data.MapNode;
 import org.osm2world.map_elevation.data.GroundState;
 import org.osm2world.math.VectorXYZ;
 import org.osm2world.math.shapes.TriangleXYZ;
+import org.osm2world.scene.color.Color;
+import org.osm2world.scene.material.BlankTexture;
+import org.osm2world.scene.material.Material;
+import org.osm2world.scene.material.TextureLayer;
 import org.osm2world.scene.mesh.Mesh;
 import org.osm2world.scene.mesh.MeshOrMeshWithMetadata;
+import org.osm2world.scene.texcoord.NamedTexCoordFunction;
+import org.osm2world.scene.texcoord.TexCoordUtil;
 import org.osm2world.world.data.NoOutlineNodeWorldObject;
 import org.osm2world.world.data.ProceduralWorldObject;
 import org.osm2world.world.data.WorldObject;
@@ -55,8 +59,12 @@ public class TestWorldModule extends AbstractModule {
 
 			VectorXYZ base = node.getPos().xyz(0);
 
+			Material material = new Material(Material.Interpolation.FLAT, Color.WHITE, Material.Transparency.FALSE,
+					List.of(new TextureLayer(BlankTexture.INSTANCE,null, null, null, false)));
+
 			TriangleXYZ triangle = new TriangleXYZ(base, base.add(0, 1, 0), base.add(1, 1, 0));
-			target.drawTriangles(PLASTIC.defaultAppearance(), singletonList(triangle), emptyList());
+			List<TriangleXYZ> ts = singletonList(triangle);
+			target.drawTriangles(material, ts, TexCoordUtil.triangleTexCoordLists(ts, material, NamedTexCoordFunction.SLOPED_TRIANGLES));
 
 		}
 
